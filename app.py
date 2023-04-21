87e66a11b7948aa37ced184154804c2d6954e031
Scott Guyton <kiljoy001@gmail.com>
2023-04-20 17:11:11 -0700

    Updated pre-commit config

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 """
 This module is the main web app loop
 """
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from src.database.database import DatabaseOperation
 
 app = Flask(__name__)
+database = DatabaseOperation("contacts.db")
 
 
 @app.route("/")
@@ -13,6 +15,19 @@ def home():
     @return: str
     """
     return render_template("index.html")
+
+
+@app.route("/ContactMe", methods=["GET", "POST"])
+def contact_me():
+    """
+    Returns contact page template & posts form data to the web app.
+    @return:
+    """
+    if request.method == "POST":
+        database.insert_contact_data(request.form)
+        return render_template("contact.html")
+    else:
+        return render_template("contact.html")
 
 
 if __name__ == "__main__":
